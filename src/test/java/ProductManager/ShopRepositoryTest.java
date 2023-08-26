57f76600f5dc02f9785a91ebf6f1e299cba2d617
Anna Var <juuni24@gmail.com>
2023-08-26 16:47:39 +0200

    Initial commit

---
+++ src/test/java/ProductManager/ShopRepositoryTest.java
@@ -0,0 +1,43 @@
+package ProductManager;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopRepositoryTest {
+    @Test
+    public void removeWhenProductExistTest() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(11, "телефон", 30_000);
+        Product product2 = new Product(22, "книга", 300);
+        Product product3 = new Product(300, "машина", 3_000_000);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+        repo.remove(22);
+        Product[] actual = repo.findAll();
+        Product[] expected = {product1, product3};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void removeWhenProductNotExistTest() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(11, "телефон", 30_000);
+        Product product2 = new Product(22, "книга", 300);
+        Product product3 = new Product(300, "машина", 3_000_000);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Assertions.assertThrows(NotFoundException.class,
+                () -> repo.remove(4444)
+                );
+
+    }
+
+}
