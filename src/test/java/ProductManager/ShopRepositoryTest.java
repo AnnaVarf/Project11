4e3e2d366cdf5d9ab7d9c900f6f71ba3d1f050da
Anna Var <juuni24@gmail.com>
2023-08-26 17:25:12 +0200

    Initial commit

--- src/test/java/ProductManager/ShopRepositoryTest.java
+++ src/test/java/ProductManager/ShopRepositoryTest.java
@@ -9,14 +9,14 @@ class ShopRepositoryTest {
     @Test
     public void removeWhenProductExistTest() {
         ShopRepository repo = new ShopRepository();
-        Product product1 = new Product(11, "телефон", 30_000);
-        Product product2 = new Product(22, "книга", 300);
-        Product product3 = new Product(300, "машина", 3_000_000);
+        Product product1 = new Product(15, "планшет", 20_000);
+        Product product2 = new Product(25, "тетрадь", 200);
+        Product product3 = new Product(500, "мопед", 2_000_000);
 
         repo.add(product1);
         repo.add(product2);
         repo.add(product3);
-        repo.remove(22);
+        repo.remove(25);
         Product[] actual = repo.findAll();
         Product[] expected = {product1, product3};
 
@@ -26,16 +26,16 @@ class ShopRepositoryTest {
     @Test
     public void removeWhenProductNotExistTest() {
         ShopRepository repo = new ShopRepository();
-        Product product1 = new Product(11, "телефон", 30_000);
-        Product product2 = new Product(22, "книга", 300);
-        Product product3 = new Product(300, "машина", 3_000_000);
+        Product product1 = new Product(15, "планшет", 20_000);
+        Product product2 = new Product(25, "тетрадь", 200);
+        Product product3 = new Product(500, "мопед", 2_000_000);
 
         repo.add(product1);
         repo.add(product2);
         repo.add(product3);
 
         Assertions.assertThrows(NotFoundException.class,
-                () -> repo.remove(4444)
+                () -> repo.remove(555)
                 );
 
     }
